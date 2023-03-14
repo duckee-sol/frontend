65bca1c47c0630239200fde9b663bf79b38bb381
therne <retail3210@gmail.com>
2023-03-15 02:42:12 +0900

    feat: Add more resource

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,14 @@
+import localFont from 'next/font/local';
 import { ThemeProvider } from '~/components/theme-provider';
 import { Inter as FontSans } from 'next/font/google';
 import React from 'react';
 import { Toaster } from '~/components/ui/toaster';
 import { cn } from '~/lib/utils';
 import '~/styles/globals.css';
+
+const objektSans = localFont({
+  src: [{ weight: '400', path: '../../public/PPObjectSans-Regular.woff2', style: 'normal' }],
+});
 
 const fontSans = FontSans({
   subsets: ['latin'],
@@ -20,7 +25,13 @@ export default function RootLayout({ children }: RootLayoutProps) {
     <>
       <html lang="en" suppressHydrationWarning>
         <head />
-        <body className={cn('min-h-screen bg-black font-sans text-slate-50 antialiased', fontSans.variable)}>
+        <body
+          className={cn(
+            'min-h-screen bg-black font-sans text-slate-50 antialiased',
+            fontSans.variable,
+            objektSans.className,
+          )}
+        >
           <ThemeProvider attribute="class" defaultTheme="dark">
             <div className="h-screen">{children}</div>
             <Toaster />
