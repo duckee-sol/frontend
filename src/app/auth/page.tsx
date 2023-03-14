2d71e724f24255fe6fb28a225f4301ac5d14351c
therne <retail3210@gmail.com>
2023-03-15 04:44:18 +0900

    fix: Fix login flow

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,8 +1,10 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { SafeEventEmitterProvider } from '@web3auth/base';
+import { Web3Auth } from '@web3auth/modal';
+import { useCallback, useEffect, useState } from 'react';
 import { useWeb3Auth } from '~/hooks/use-web3auth';
-import { returnToAppWithData } from '~/lib/in-app-browser';
+import { closeInAppBrowser, returnToAppWithData } from '~/lib/in-app-browser';
 import RPC from '../../solanaRPC';
 import './web3auth-custom.css';
 
@@ -10,33 +12,41 @@ export default function App() {
   const { web3auth, provider } = useWeb3Auth();
   const [isModalShown, setModalShown] = useState(false);
 
+  const handleAfterLogin = useCallback(async (web3auth: Web3Auth, provider: SafeEventEmitterProvider) => {
+    const { idToken } = await web3auth.authenticateUser();
+    console.log('ID Token: ' + idToken);
+
+    const rpc = new RPC(provider);
+    const addresses = await rpc.getAccounts();
+
+    console.log(`[${addresses.join(', ')}] addresses found. Returning back to app`);
+    returnToAppWithData('auth_result', {
+      idToken,
+      address: addresses[0],
+    });
+  }, []);
+
   useEffect(() => {
     if (!web3auth) {
       return;
     }
     if (web3auth && provider) {
       // handle after login
-      (async () => {
-        if (!provider || !web3auth) {
-          return;
-        }
-        const { idToken } = await web3auth.authenticateUser();
-        console.log('ID Token: ' + idToken);
-
-        const rpc = new RPC(provider);
-        const addresses = await rpc.getAccounts();
-
-        console.log(`[${addresses.join(', ')}] addresses found. Returning back to app`);
-        returnToAppWithData('auth_result', {
-          idToken,
-          address: addresses[0],
-        });
-      })();
+      handleAfterLogin(web3auth, provider).catch(console.error);
       return;
     }
     if (!isModalShown) {
       setModalShown(true);
-      web3auth.connect().catch(console.error);
+      web3auth
+        .connect()
+        .then(async (provider) => {
+          if (!provider) {
+            closeInAppBrowser();
+            return;
+          }
+          await handleAfterLogin(web3auth, provider);
+        })
+        .catch(console.error);
     }
   }, [web3auth, isModalShown, provider]);
 
